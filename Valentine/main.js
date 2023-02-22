41f3f8469f9ed52f8356ab45e08e6c5c0976c7d0
Hoàng Đặng Xuân <dxh4402@gmail.com>
2023-02-22 15:29:37 +0700

    upload new code

---
+++ Valentine/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.container').mouseenter(function () {
+        $('.card').stop().animate({
+            top: '-90px'
+        }, 'slow');
+    }).mouseleave(function () {
+        $('.card').stop().animate({
+            top: 0
+        }, 'slow');
+    });
+});
